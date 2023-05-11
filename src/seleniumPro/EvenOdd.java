eb0c42d576aed69e6d6086237a15ed4e422d7459
mangesh33 <mh.lat.mangesh@gmail.com>
2023-05-11 00:38:19 -0700

    this is my first commit

---
+++ src/seleniumPro/EvenOdd.java
@@ -0,0 +1,43 @@
+package seleniumPro;
+
+
+public class EvenOdd {
+
+	public static void main(String[] args)  {
+
+
+	      
+	     int num=1;
+	     
+	     if(num%2==0)
+	     {
+	    	 System.out.println("even");
+	     }
+	     
+	     else
+	     {
+	    	 System.out.println("odd");
+
+	     }
+	    	 
+	       
+	       
+	       
+	       
+	       
+	       
+	       
+	       
+	       
+	       
+	       
+	       
+	       
+		
+		
+		
+		
+
+	}
+
+}
---
+++ src/seleniumPro/PositiveNum.java
@@ -0,0 +1,35 @@
+package seleniumPro;
+
+public class PositiveNum {
+
+	public static void main(String[] args) {
+
+		
+		int num=0;
+		
+		if(num>0)
+		{
+			System.out.println("positive number");
+		}
+		
+		else if(num<0)
+		{
+			System.out.println("negative number");
+	
+		}
+		
+		else
+			
+		{
+			System.out.println("zero number");
+	
+		}
+		
+		
+		
+		
+		
+		
+	}
+
+}
